999d5a79998766d7d15bc7c1267c04eb37d4eda3
Wezz Balk <wezz@wezz.se>
2016-12-02 09:35:21 +0100

    Handlebar helpers does not have support for multiple target css files

--- assemble-helpers/assemble-helper-css-name.js
+++ assemble-helpers/assemble-helper-css-name.js
@@ -8,7 +8,25 @@ module.exports.register = function (Handlebars, options, params) {
   var gruntFileConfig = basePath + "/gruntfileConfig.json";
   var config = grunt.file.readJSON(gruntFileConfig);
 
-  Handlebars.registerHelper("cssFileName", function() {
-    return config.css.fileName + ".css";
+  Handlebars.registerHelper("cssFileName", function ()
+  {
+  	return (config.css.fileName) ? config.css.fileName + ".css" : null;
   })
+
+  Handlebars.registerHelper("cssFiles", function ()
+  {
+  	var files = "";
+  	if (typeof config.css.fileName === "string")
+  	{
+  		files += '<link href="' + this.assets +'/css/'+ config.css.fileName +'.css" rel="stylesheet" />\n';
+  	}
+  	if (typeof config.css.targets === "object")
+  	{
+  		for (let key in config.css.targets)
+  		{
+  			files += '<link href="' + this.assets + '/css/' + key + '.css" rel="stylesheet" />\n';
+  		}
+  	}
+  	return files;
+  });
 };
